03551069aa0ac2afa50a32e27f289d2ec66075db
willownsenator <willownsenator@gmail.com>
2024-04-11 21:37:22 +0200

    Add SpaceBear nft to foundry

---
+++ foundry/src/Spacebear.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Spacebear is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("Spacebear", "SBR") Ownable(msg.sender) {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return
+            "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function buyToken() public payable {
+        uint256 tokenId = _nextTokenId++;
+        require(msg.value == tokenId * 0.1 ether, "Not enough funds sent");
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
